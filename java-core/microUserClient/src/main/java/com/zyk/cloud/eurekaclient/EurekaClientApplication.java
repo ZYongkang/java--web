07452486ee55e0aff4e07a42f02307180183385d
zhangyongkang <1479834277@qq.com>
2019-07-25 16:30:33 +0800

    wangle

--- java-core/microUserClient/src/main/java/com/zyk/cloud/eurekaclient/EurekaClientApplication.java
+++ java-core/microUserClient/src/main/java/com/zyk/cloud/eurekaclient/EurekaClientApplication.java
@@ -3,7 +3,6 @@ package com.zyk.cloud.eurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
--- java-core/microUserClient/src/main/java/com/zyk/cloud/eurekaclient/controller/UserController.java
+++ java-core/microUserClient/src/main/java/com/zyk/cloud/eurekaclient/controller/UserController.java
@@ -3,6 +3,7 @@ package com.zyk.cloud.eurekaclient.controller;
 import com.zyk.cloud.eurekaclient.model.MicroBeanObject;
 import com.zyk.cloud.eurekaclient.model.UserDO;
 import com.zyk.cloud.eurekaclient.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Autowired
